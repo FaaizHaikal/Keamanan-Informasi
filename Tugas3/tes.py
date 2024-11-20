055821e9335c4e63bc6f21f5d0b0e22684936381
FaaizHaikal <faaizhaikal@gmail.com>
2024-11-20 17:34:21 +0700

    feat: add testing

--- Tugas3/tes.py
+++ Tugas3/tes.py
@@ -1,19 +1,40 @@
 import socket
+import json
+import os
 
-HOST='127.0.0.0'
+from utils.mitecom import PublicKeyRequest, read_all
+
+HOST='127.0.0.1'
 PORT=12345
+
+dir = os.path.dirname(__file__)
+file = os.path.join(dir, "utils/pub_keys/pka.pem")
+with open(file, "r") as f:
+  PUBLIC_KEY = tuple(map(int, f.read().split(",")))
+
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
+def decrypt(ciphertext):
+    d, n = PUBLIC_KEY
+    plain = [chr(pow(char, d, n)) for char in ciphertext]
+    return ''.join(plain)
+
 def send_request(data):
-  client.sendall(data.encode())
+  client.sendall(data.encode('utf-8'))
   print(f"Sent: {data}")
-  
-  # response = client.recv(1024).decode()
-  
-# send json string
-data = '{"name": "John Doe", "age": 30}'
-send_request(data)
+
+request = PublicKeyRequest("brob", "initiator")
+send_request(json.dumps(request.to_msg()))
+
+response = read_all(client)
+ciphertext = json.loads(response)
+
+print(f"Received: {ciphertext}")
+
+decrypted_response = decrypt(ciphertext)
+
+print(f"Decrypted Response: {decrypted_response}")
 
 # client.close()
