612390eb92eb5550db11a8fd6bae2a3195910c8b
FaaizHaikal <faaizhaikal@gmail.com>
2024-11-20 15:01:20 +0700

    feat: add RSA class

---
+++ Tugas3/utils/RSA.py
@@ -0,0 +1,42 @@
+import os
+
+class RSA:
+  def __init__(self, node):
+    self.node = node
+    self.read_keys()
+    
+  def read_keys(self):
+    dir = os.path.dirname(__file__)
+    public_key_path = os.path.join(dir, f"pub_keys/{self.node}.pem")
+    
+    with open(public_key_path, "r") as f:
+      self.public_key = tuple(map(int, f.read().split(",")))
+      
+    private_key_path = os.path.join(dir, f"priv_keys/{self.node}.pem")
+    
+    with open(private_key_path, "r") as f:
+      self.private_key = tuple(map(int, f.read().split(",")))
+    
+  def encrypt(self, plaintext):
+    e, n = self.public_key
+    cipher = [pow(ord(char), e, n) for char in plaintext]
+    return cipher
+
+  def decrypt(self, ciphertext):
+    d, n = self.private_key
+    plain = [chr(pow(char, d, n)) for char in ciphertext]
+    return ''.join(plain)
+
+if __name__ == "__main__":
+  rsa = RSA("initiator")
+  
+  plaintext = "Hello, World!"
+  ciphertext = rsa.encrypt(plaintext)
+  
+  print("Plain Text:", plaintext)
+  print("Cipher Text:", ciphertext)
+  
+  decrypted_msg = rsa.decrypt(ciphertext)
+  
+  print("Decrypted Message:", decrypted_msg)
+  
